bea82b3ed8025ac00c0b4994f9f587014d46fb8f
jawkat <kad.joliano@gmail.com>
2024-05-02 10:52:08 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # Puppet Manifest to  install flask from pip3.
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
 package { 'flask':
   ensure   => '2.1.0',   # Specify the desired version of Flask.
   provider => 'pip3',    # Use pip3 as the package provider.
-  require  => Package['python3-pip'], # Ensure pip3 is installed before installing Flask.
 }
