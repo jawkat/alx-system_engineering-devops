ed62350d4f26757cf6733b3f79452d2db74192fb
jawkat <kad.joliano@gmail.com>
2024-06-04 16:57:33 +0100

    without timeout

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     """ Construct the URL for the subreddit's about page in JSON format """
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers={'User-Agent': 'app/1.0'}, timeout=2)
+    response = requests.get(url, headers={'User-Agent': 'app/1.0'})
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
